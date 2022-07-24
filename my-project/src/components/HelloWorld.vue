07b8ee5f2510ca845ab2d6447c682fb22f832778
ghkdsigm3 <ghkdsigm3@gmail.com>
2022-07-24 16:07:13 +0900

    first commit

---
+++ my-project/src/components/HelloWorld.vue
@@ -0,0 +1,46 @@
+<script>
+import { reactive, ref } from 'vue'
+
+export default {
+  setup() {
+    let ss = ref(0)   
+    const zz = reactive(null)
+    let count = function(){
+      this.ss++
+      
+      if(this.ss == 20){
+        alert('20됏음')
+      }
+
+      if(this.ss%2 == 1){
+        this.zz.style.color = 'red';
+      } else {
+        this.zz.style.color = '#fff';
+      }
+    }
+    return {
+      count,
+      ss,
+      zz      
+    }
+  },
+}
+</script>
+
+<template>
+  <div class="card">
+    <button type="button" @click="count">count is</button>
+    <p>
+      곱하기 {{ ss }}
+    </p>
+    <p ref="zz">
+      shoot
+    </p>
+  </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
