03900e53aa5022b072653e0ca44a0f940d55494c
ghkdsigm3 <ghkdsigm3@gmail.com>
2022-07-24 16:52:49 +0900

    onmounted 연습

--- my-project/src/components/HelloWorld.vue
+++ my-project/src/components/HelloWorld.vue
@@ -6,29 +6,38 @@ export default {
     let ss = ref(0)   
     const zz = ref(null)
     const el = ref()
+    const kkk = ref()
 
     let count = function(){
       this.ss++
       
-      if(this.ss == 20){
-        alert('20됏음')
+      if(this.ss == 5){
+        alert('5됏음')
+        this.kkk.classList.remove('text-5xl');
       }
       if(this.ss%2 == 1){
-        this.zz.style.color = 'red';
+        this.zz.style.color = 'red';        
       } else {
         this.zz.style.color = '#fff';
       }      
     }
 
+    // const kk = function(){
+    //   this.kkk.value.classList.add('text-6xl');
+    // }
+
     onMounted(()=>{
       el.value.style.color = 'blue';
+      kkk.value.classList.add('text-5xl');
+      console.log('aaaa')
     })
     
     return {
       count,
       ss,
       zz,
-      el
+      el,
+      kkk,
     }
   },
 }
@@ -46,11 +55,33 @@ export default {
     <p ref="el">
       shoots
     </p>
+    <p class=" font-bold " ref="kkk"> 
+      <!-- text-2xl -->
+      adsfasdf
+    </p>
   </div>
 </template>
 
 <style scoped>
 .read-the-docs {
   color: #888;
+}
+button {
+  border-radius: 8px;
+  border: 1px solid transparent;
+  padding: 0.6em 1.2em;
+  font-size: 1em;
+  font-weight: 500;
+  font-family: inherit;
+  background-color: #1a1a1a;
+  cursor: pointer;
+  transition: border-color 0.25s;
+}
+button:hover {
+  border-color: #646cff;
+}
+button:focus,
+button:focus-visible {
+  outline: 4px auto -webkit-focus-ring-color;
 }
 </style>
