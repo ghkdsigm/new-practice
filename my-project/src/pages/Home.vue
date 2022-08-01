3493732809408b22afd3e3ae98e54a793b26aab7
ghkdsigm3 <ghkdsigm3@gmail.com>
2022-08-01 16:14:08 +0900

    공부

--- my-project/src/pages/Home.vue
+++ my-project/src/pages/Home.vue
@@ -1,12 +1,36 @@
 <template>
-  <div>111</div>
+  <div>
+    <input type="text" v-model="insert" @keyup.enter="actionKey" placeholder="seacrh something">
+    <div>
+      {{insert}}
+    </div>
+  </div>
 </template>
 
 <script>
+  import { ref,onBeforeMount,onMounted, computed, onBeforeUnmount } from 'vue'
+  import { useStore, mapGetters } from 'vuex'
 export default {
-    components:{
-        
+
+  components:{
+      
+  },
+  setup(){
+    let insert = ref()
+    const actionKey = () => {
+      alert('2')
+      insert.value = ''
     }
+    return{
+      insert,
+      actionKey
+    }
+  },
+  // methods:{
+  //   actionKey(){
+  //     alert(1)
+  //   }
+  // }
 }
 </script>
 
